e1b6dc34e7cccc1b173ae64fe2274efccf864718
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-07-04 08:45:40 +0530

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample React app using Yarn.</p>
+    </div>
+  );
+}
+
+export default App;
+
